40d857e10f3c67919d59d8b8b5dcf653d4a720ce
woofdoggo <woofwoofdoggo@protonmail.com>
2022-03-08 15:43:07 -0500

    Fix scan_depth setting

--- lua/lsp-project.lua
+++ lua/lsp-project.lua
@@ -2,7 +2,7 @@ local plenary = require("plenary.async")
 
 local M = {}
 
-local conf_depth = 1
+local conf_depth = -1
 
 -- Helper function used for recursively
 -- merging two tables.
@@ -28,7 +28,7 @@ local function read_and_apply(client)
     local dir = vim.fn.getcwd()
     local sep = package.config:sub(1, 1)
 
-    while depth <= conf_depth do
+    while depth <= conf_depth or conf_depth == -1 do
         -- check for .lspconfig.json
         local conf_path = dir .. sep .. ".lspconfig.json"
 
@@ -72,8 +72,8 @@ function M.setup(config)
         error("lsp-project: config was not a table")
     end
 
-    if config.depth ~= nil and type(config.depth) == "number" then
-        conf_depth = config.depth
+    if config.scan_depth ~= nil and type(config.scan_depth) == "number" then
+        conf_depth = config.scan_depth
     end
 end
 
